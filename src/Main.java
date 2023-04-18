8a04613452312c9b2b6500390f3339ed6f1108db
Kristina Marienfeld <kristina@MacBook-Air-von-Kristina.local>
2023-04-18 10:15:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+
+public static void main(String[] args) {
+    /*int nr1 = 5;
+    int nr2 = 3;
+
+    //string summe
+
+    System.out.println(nr1+nr2);
+
+    int summe = nr1 + nr2;
+    System.out.println(Integer.toString(summe));
+
+    int x = 5, y = 7;
+
+    System.out.println("x + y: " + x + y);
+    System.out.println("x - y: " + (x - y));
+    System.out.println("x * y: " + x * y);
+    System.out.println("x / y: " + x % y);
+
+
+    System.out.println("X < Y: " + (x < y) );
+    System.out.println("X > Y: " + (x > y));
+    System.out.println("x = y: " + (x == y));
+
+if (x > y) {
+    System.out.println("x ist größer y");
+
+} else if (x < y) {
+    System.out.println("x ist kleiner y");
+}else {
+    System.out.println("x ist gleich y");
+}
+    double i = 5.2, p = 7.3;
+
+    System.out.println("x + y: " + i + p);
+    System.out.println("x - y: " + (i - p));
+    System.out.println("x * y: " + i * p);
+    System.out.println("x / y: " + i % p); */
+
+    int[] testArray = new int[3];
+
+    testArray[0] = 3;
+    testArray[1] = 4;
+    testArray[2] = 5;
+
+    System.out.println(testArray[0] + testArray[2]);
+
+    int summe = testArray[0] + testArray[1];
+
+    System.out.println(summe);
+
+    System.out.println(testArray.length);
+
+}
+
+}
+
+
+
+
+
+
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+/*public class Main {
+    public static void main(String[] args) {
+        int[][] MULTIPLICATION_TABLE;
+        MULTIPLICATION_TABLE = new int[10][10];
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                MULTIPLICATION_TABLE[i - 1][j - 1] = i * j;
+                System.out.print(MULTIPLICATION_TABLE[i - 1][j - 1]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
